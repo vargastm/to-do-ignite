24ed195ad1623a9ab66d3a1ff91431dad7325d4a
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-07-04 13:29:18 -0300

    created empity page

--- src/components/Body/Index.tsx
+++ src/components/Body/Index.tsx
@@ -1,11 +1,30 @@
-import { PlusCircle } from "phosphor-react";
-import { Container } from "./styles";
+import { ClipboardText, PlusCircle } from "phosphor-react";
+import { Container, Content } from "./styles";
 
 export function Body() {
   return (
     <Container>
-      <input type="text" placeholder="Adicione uma nova tarefa"/>
-      <button>Criar<PlusCircle size={16} /></button>
+      <div>
+        <input type="text" placeholder="Adicione uma nova tarefa"/>
+        <button>Criar<PlusCircle size={16} /></button>
+      </div>
+      <Content>
+        <header>
+          <div>
+            Tarefas Criadas
+            <span>0</span>
+          </div>
+          <div>
+            Concluidas
+            <span>0</span>
+          </div>
+        </header>
+        <div>
+          <ClipboardText size={72} />
+          <span>Você ainda não tem tarefas cadastradas</span>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
+      </Content>
     </Container>
   )
 }
--- src/components/Body/styles.ts
+++ src/components/Body/styles.ts
@@ -3,33 +3,87 @@ import styled from 'styled-components'
 export const Container = styled.body`
   background: #1A1A1A;
   height: calc(100vh - 192px);
-  display: flex;
-  justify-content: center;
-  gap: 0.5rem;
 
-  input {
-    margin-top: -1.6875rem;
-    height: 3.375rem;
+  > div {
+    display: flex;
+    gap: 0.5rem;
     width: 46rem;
-    padding-left: 1rem;
-    color: var(--gray-300);
-    background: var(--gray-500);
-    border: solid 1px var(--gray-700);
-    border-radius: 8px;
+    margin: 0 auto;
+
+    input {
+      width: 100%;
+      margin-top: -1.6875rem;
+      height: 3.375rem;
+      padding-left: 1rem;
+      color: var(--gray-300);
+      background: var(--gray-500);
+      border: solid 1px var(--gray-700);
+      border-radius: 8px;
+    }
+
+    button {
+      margin-top: -1.6875rem;
+      height: 3.25rem;
+      width: 5.625rem;
+      background: var(--blue-dark);
+      color: var(--gray-100);
+      border: none;
+      border-radius: 8px;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 8px;
+    }
+  }
+`
+
+  export const Content = styled.section`
+  border-top: 1px solid var(--gray-400);
+  width: 46rem;
+  margin: 4rem auto;
+  border-radius: 8px;
+
+
+  header {
+    margin-top: 1.5rem;
+    display: flex;
+    justify-content: space-between;
+
+    div {
+      font-size: 0.875rem;
+      font-weight: 700;
+
+      &:first-child {
+        color: var(--blue);
+      }
+
+      &:last-child {
+        color: var(--purple);
+      }
+    }
+
+    span {
+      background: var(--gray-400);
+      margin-left: 8px;
+      padding: 0 8px;
+      border-radius: 8px;
+    }
   }
 
-  button {
-    margin-top: -1.6875rem;
-    height: 3.25rem;
-    width: 5.625rem;
-    background: var(--blue-dark);
-    color: var(--gray-100);
-    border: none;
-    border-radius: 8px;
-    cursor: pointer;
+  > div {
+    color: var(--gray-300);
     display: flex;
+    flex-direction: column;
+    width: 46rem;
+    margin: 0 auto;
     align-items: center;
-    justify-content: center;
-    gap: 8px;
+    margin-top: 5.5rem;
+    line-height: 140%;
+    
+    span {
+      font-weight: 700;
+      margin-top: 16px;
+    }
   }
 `
